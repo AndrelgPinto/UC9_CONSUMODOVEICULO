a6cb33dde6672499bf89145b6a9c82a4c192e601
andre.lgpinto <andregomeees0@gmail.com>
2023-03-01 21:56:46 -0300

    Criando Repositório para CONSUMODOVEICULO

---
+++ UC9_CONSUMODOVEICULO/Program.cs
@@ -0,0 +1,24 @@
+﻿int adultos, criancas, animais;
+double valoraluguel, valorfinal;
+
+adultos = 0;
+criancas = 0;
+animais = 0;
+valoraluguel = 0;
+valorfinal = 0;
+
+Console.WriteLine("Qual o valor atual do aluguel?");
+valoraluguel = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Quantidade de adultos na casa?");
+adultos = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Quantidade de criancas na casa?");
+criancas = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Quantidade de animais na casa?");
+animais = int.Parse(Console.ReadLine());
+
+valorfinal = valoraluguel - (adultos * (valoraluguel * 0.0125)) - (criancas * (valoraluguel * 0.0107)) + (animais * (valoraluguel * 0.02));
+
+Console.WriteLine("O valor final do aluguel é: " + valorfinal);
